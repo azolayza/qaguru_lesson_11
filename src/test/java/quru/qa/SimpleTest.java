1ff7b2deef2a40443d72f173008addab0a16e143
Elizaveta Azovtseva <azovcevae@gmail.com>
2022-01-30 17:06:47 +0700

    add second test assertion2

---
+++ src/test/java/quru/qa/SimpleTest.java
@@ -0,0 +1,12 @@
+package quru.qa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+
+    @Test
+    void testAssret(){
+        Assertions.assertTrue(3>2);
+    }
+}
